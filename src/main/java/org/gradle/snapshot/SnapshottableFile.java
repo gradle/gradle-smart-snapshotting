56d977ecf96765d742aaff16a3c6878b2d507cd2
Stefan Wolf <wolf@gradle.com>
2017-03-31 09:51:13 +0200

    Add Javadoc for open

--- src/main/java/org/gradle/snapshot/SnapshottableFile.java
+++ src/main/java/org/gradle/snapshot/SnapshottableFile.java
@@ -3,6 +3,10 @@ package org.gradle.snapshot;
 import java.io.InputStream;
 
 public interface SnapshottableFile {
+    /**
+     * Opens the input stream. This is only guaranteed to work once.
+     * The caller needs to make sure to close the input stream.
+     */
     InputStream open();
     String getPath();
     FileType getType();
